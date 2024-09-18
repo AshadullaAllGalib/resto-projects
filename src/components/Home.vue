530a501fb016afeae217a066c7bc489b467823d5
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-09-18 20:36:19 +0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Hello User, Welcome on Home page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (!user) {
+            this.$router.push({ name: "SignUp" })
+        }
+    },
+
+}
+</script>
+
+<style scoped></style>
