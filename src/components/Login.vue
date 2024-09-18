3ac20c2479a5979b0ceadaadfc5fa815ff66e3e9
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-09-18 22:15:24 +0600

    update login page for workable and add to routes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,7 +10,7 @@
             <!-- <input type="text" v-model="name" placeholder="Enter Name"> -->
             <input type="email" v-model="email" placeholder="Enter Email">
             <input type="password" v-model="password" placeholder="Enter Password">
-            <button>Login</button>
+            <button v-on:click="login">Login</button>
 
             <div class="user-login">
                 <p>Not Yet Registered</p>
@@ -22,8 +22,38 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    name: 'Login'
+    name: 'Login',
+
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        async login() {
+            let result = await axios.get(
+                `http://localhost:3000/user?email=${this.email}&${this.password}`
+            )
+            console.warn(result);
+
+            if (result.status = 200 && result.data.length > 0) {
+                localStorage.setItem("user-info", JSON.stringify(result.data[0]))
+                this.$router.push({ name: "Home" })
+            }
+
+        }
+    },
+
+    mounted() {
+        let user = localStorage.getItem('user-info');
+        if (user) {
+            this.$router.push({ name: "Home" })
+        }
+    }
 
 }
 </script>
